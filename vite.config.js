330d6747dd84ec4125924b3228cd84bda6fecf76
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2023-01-22 11:08:42 +0700

    chore: setup react plugin on vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+    base: command === "serve" ? "" : "build",
+    build: {
+        manifest: true,
+        outDir: "public/build",
+        rollupOptions: {
+            input: "resources/js/app.js",
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
+        reactRefresh(),
     ],
-});
+}));
